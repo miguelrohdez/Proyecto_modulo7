a05d649c1330f251274877c829cbd96784f9d801
edurock <rockecg6@gmail.com>
2021-06-04 14:47:45 -0500

    Update verifica_datos.php

--- php/verifica_datos.php
+++ php/verifica_datos.php
@@ -6,24 +6,31 @@
 </head>
 <body>
     <?php 
+
         require("datos_con.php");
         $usuario =   $_POST["txt_email"];
         $password = $_POST["txt_contra"];
-        $conexion = new mysqli($db_host, $db_admin,$db_pass,$db_data);
+        $conexion = mysqli_connect("localhost", "root", "usbw");
+        mysqli_select_db($conexion, "lalos_buger");
+        
         if ($conexion -> connect_errno) {
             echo "Fallo la conexion ".$conexion -> connect_errno;
         }else{
             $conexion -> set_charset("utf8");
-            $consulta = "SELECT * FROM cliente WHERE cliente_correo = '$usuario' AND cliente_contrasenia = '$conexion'";
-            $resultado = $conexion -> query($consulta);
+            $consulta = "SELECT * FROM cliente WHERE cliente_correo = '$usuario' AND cliente_contrasenia = '$password'";
+            $resultado = mysqli_query($conexion, $consulta);
             $registros = $conexion -> affected_rows;
-            echo $conexion -> fied_count;
+            //echo $conexion -> fied_count;
             if ($registros > 0) {
                 echo "Es valido";
+                $fila = mysqli_fetch_array($resultado); 
+                echo "<br>- Nombre: ". $fila['cliente_nombre']. "<br>";
+                echo "- Correo: ". $fila['cliente_correo']. "<br>";
             }else{
-
-                header("location:login.html"); //Posiblemente agrege una loginErro.html con el mensaje de error
+                echo "<h3>Error de autentifiacion</h3>";
+                echo "<a href='/Proyecto/login.html'>Volver </a>"; //Posiblemente agrege una loginErro.html con el mensaje de error
             }
+            mysqli_free_result($resultado);
             $conexion -> close();
         }            
     
