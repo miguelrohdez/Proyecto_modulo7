49f0d2e962d69e815d9c437733b8a3bdf687ac86
Miguel Alejandro Rojas Hernández <nomeacuerdox4@gmail.com>
2021-06-05 04:40:38 -0500

    funcionaLoginConNav

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="es-ES">
+	<head>
+		<meta charset="utf-8">
+		<title>Lalo's Burgers</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+		
+		
+	</head>
+	<body>
+	<!-- Parte que verifica el login -->
+		<?php
+		if (isset($_POST["btn_entrar"])) {
+			
+			require("./php/datos_con.php");
+			$usuario =   $_POST["txt_email"];
+			$password = $_POST["txt_contra"];
+			$conexion = new mysqli($db_host, $db_admin,$db_pass,$db_data,$db_port);
+			if ($conexion -> connect_errno) {
+				echo "Fallo la conexion ".$conexion -> connect_errno;
+			}else{
+				$conexion -> set_charset("utf8");
+				$consulta = "SELECT cliente_nombre FROM cliente WHERE cliente_correo = ? AND cliente_contrasenia = ?";
+				$stmt = $conexion->prepare($consulta);
+            	$stmt->bind_param("ss", $usuario, $password);
+            	$stmt->execute();	
+				$stmt->bind_result($nombre_cliente);
+
+				if ($stmt->fetch() ) {
+					session_start();
+					$_SESSION["usuario"]=$nombre_cliente;
+					//header("location:carrito.php"); NO SE A QUE PAGINA MARDAR AL CLIENTE REGISTRADO
+				}else{
+					header("index.php"); //Posiblemente agrege una loginErro.html con el mensaje de error
+				}
+				$conexion -> close();
+			}            
+		} 
+			?>
+		<!-- Aqui  empieza la barra de menus y logo -->
+		<?php 
+				if (!isset($_SESSION["usuario"])) {
+					include("nav_nologin.html");
+				}else{
+					include("nav_login.html");
+				}
+		?>
+	
+	<!-- Aqui  empieza la caja principal -->
+	<div class="container">
+		<header>
+			<h1 class="tituloPrincipal">Bienvenido a Lalo's Burgues</h1>
+		</header>	
+		<!-- Aqui  empieza la seccion del formulario o datos a mostrar -->
+		<div class="caja principal">
+			
+			<!-- LOGIN
+			CORREO
+					CONTRA
+					BTN INGRESAR (VERIFICAR) Y BTN REGISTRAR (IR A REGISTRAR) -->
+			<?php 
+				if (!isset($_SESSION["usuario"])) {
+					include("formulario_login.html");
+				}else{
+					echo "<h2>Es bueno verte de nuevo " .$_SESSION["usuario"]."</h2>";
+				}
+			?>
+			<center>
+				<img  id="img_index" src="./img/restaurante.jpg" alt="Restaurante-Lalos">
+			</center>
+			
+		</div>
+		
+		
+		<div class="clear"></div>
+		</div>
+		<footer>
+			<h1 class="text-footer"> Ⓒ 2021 Lalo's Burger ALGUNOS PRODUCTOS ESTAN SUJETOS A DISPONIBILIDAD</h1>
+		</footer>
+	</body>
+</html>
--- php/datos_con.php
+++ php/datos_con.php
@@ -2,5 +2,6 @@
     $db_host = "localhost";
     $db_data = "lalos_burger";
     $db_admin = "root";
-    $db_pass = "usbw";
+    $db_pass = "";
+    $db_port = 3306;
 ?>
