08c5e2abb2250f96ea12c0e725840b27a611b572
Julio Cesar <jc_jluis@hotmail.com>
2021-06-05 20:46:02 -0500

    v5

--- menu.php
+++ menu.php
@@ -1,16 +1,27 @@
+<?php
+   /* session_start();    
+    $varSesion = $_SESSION['usuario'];
+
+
+    if($varSesion == null || $varSesion== ""){
+        echo "No tiene acceso a esta pagina";
+        die();
+    }*/
+?>
+
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<html lang="es-ES">
+	<head>
+		<meta charset="utf-8">
+		<title>Menu</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+	</head>
+    <body>
     <!-- Aqui  empieza la barra de menus y logo -->
 		<div class="container-menu">
 			<nav class="nav-main">
-				<a class="logoNav" href="./index.html">
+				<a class="logoNav" href="index.php">
 					<img id="logo" src="img/logoFinal.png" alt="Imagen Logo">
 				</a>
 				<ul class="nav-menu">
@@ -24,7 +35,7 @@
 						<a href="#">Promociones</a>
 					</li>
 					<li>
-						<a href="#">Mi orden</a>
+						<a href="#">Carrito</a>
 					</li>
 					<li>
 						<a href="#">Contacto</a>
@@ -48,32 +59,56 @@
 				<h1 class="tituloPrincipal">MENU DE HAMBURGUESAS Y BEBIDAS</h1>
 			</header>	
 		<!-- Aqui  empieza la seccion del formulario o datos a mostrar -->
-		<div class="caja principal">
+		<div class="pedidoActual">
 				<!-- COMBOS
 					IMAGEN
 					DESCRI
 					PRECIO AGREGAR(IR A CARRITO) -->
-        
-        
 			<!--	Se inicia form, para enviar datos de pedido -->
             <form method="POST" name = "pedido" Onsubmit = "return Validation(this)" action="/proyecto/php/carrito.php">
 
-                <input type="radio" name="orden" value="1" id="o1">
-                <label for="o1">Orden 1</label><br>
 
-                <input type="radio" name="orden" value="2" id="o2">
-                <label for="o2">Orden 2</label><br>
+                <table class="menuH">
+                <tr>
+                    <td>
+                        <p> Hamburguesa sencilla </p>                    
+                        <input type="radio" name="orden" value="1" id="o1">
+                        <label for="o1">Orden 1</label><br>
+                    </td>
+                    <td>
+                        <p>Hamburguesa con queso</p>                    
+                        <input type="radio" name="orden" value="2" id="o2">
+                        <label for="o2">Orden 2</label><br>
+                    </td>
+                    <td>
+                        <p>Hamburguesa doble queso y papas</p>                    
+                        <input type="radio" name="orden" value="3" id="o3">
+                        <label for="o3">Orden 3</label><br>
+                    </td>
+                </tr><br>
+                <tr>
+                    <td>
+                        <p>Hamburguesa con queso y tosino</p>                    
+                        <input type="radio" name="orden" value="4" id="o4">
+                        <label for="o4">Orden 4</label><br>
+                    </td>
+                    <td>
+                        <p>Hamburguesa con papas</p>                    
+                        <input type="radio" name="orden" value="5" id="o5">
+                        <label for="o5">Orden 5</label><br>
+                    </td>
+                    <td>
+                        <p>Hamburguesa con papas y refresco</p>                    
+                        <input type="radio" name="orden" value="4" id="o6">
+                        <label for="o6">Orden 6</label><br>
+                    </td>
+                </tr>
+                </table>
 
-                <input type="radio" name="orden" value="3" id="o3">
-                <label for="o3">Orden 3</label><br>
-
-                <input type="radio" name="orden" value="4" id="o4">
-                <label for="o4">Orden 4</label><br>
-                
                 <br>
                 <input type="number" name="cantidad" min="1" max="50" step="1"  required="required">
 
-                <input type="submit" value="Confirmar" name="confirmar">
+                <input type="submit" value="Confirmar Cantidad" name="confirmar">
             </form>
                 
 		</div>
--- php/listarMenu.php
+++ php/listarMenu.php
@@ -2,14 +2,14 @@
     
     function listarMenu($resultados){
 		if ($resultados != NULL){
-			echo "<br/>****************************<br/><br/>";
-			echo "- Codigo: ". $resultados['codigo']."<br/> ";
-			echo "- Nombre: ". $resultados['menu_nombre']."<br/> ";
-			echo "- Precio: ". $resultados['menu_precio']."<br/> ";
-            echo "- Tipo: ". $resultados['menu_tipo']."<br/> ";
-			echo "- Descripcion: ". $resultados['menu_descripcion']."<br/> ";
-            echo "- Imagen: ". $resultados['menu_imagen']."<br/> ";
-			echo "<br/>****************************<br/><br/>";
+			echo "<br/>***********************************************<br/><br/>";
+			echo "- Numero Menu (Codigo): ". $resultados['codigo']."<br/> ";
+			echo "- Nombre Pedido: ". $resultados['menu_nombre']."<br/> ";
+			echo "- Precio Pedido: ". $resultados['menu_precio']."<br/> ";
+            echo "- Tipo de pedido: ". $resultados['menu_tipo']."<br/> ";
+			echo "- Descripcion del pedido: ". $resultados['menu_descripcion']."<br/> ";
+            echo "- Img: ". $resultados['menu_imagen']."<br/> ";
+			echo "<br/>***********************************************<br/><br/>";
 		}
 		else {echo "<br/> No hay mas datos: <br/>".$resultados; }
 	}
