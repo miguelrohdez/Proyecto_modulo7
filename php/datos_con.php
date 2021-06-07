2200aca8274fc729d4224caf14f0b273c6366683
Miguel Alejandro Rojas Hernández <nomeacuerdox4@gmail.com>
2021-06-06 21:07:12 -0500

    datos

--- php/datos_con.php
+++ php/datos_con.php
@@ -1,7 +1,7 @@
 <?php 
     $db_host = "localhost";
-    $db_data = "lalos_buger";
+    $db_data = "lalos_burger";
     $db_admin = "root";
-    $db_pass = "usbw";
-    //$db_port = 3306;
+    $db_pass = "";
+    $db_port = 3306;
 ?>
